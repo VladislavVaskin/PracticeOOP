b344bfe56be891a67e8cbc5f90b273403c4ecbb8
Vladislav Vaskin <vvaskin@inno.tech>
2026-01-23 18:46:33 +0300

    Решение для задачи Практика ООП. Инкапсуляция. Дробь

---
+++ src/main/java/Fraction.java
@@ -0,0 +1,73 @@
+public class Fraction {
+   private int numerator; //Числитель
+   private int denominator; //Знаменатель
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator <= 0 ) {
+            throw new IllegalArgumentException("Знаменатель не может быть меньше или равен нулю");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator <= 0)
+            throw new IllegalArgumentException("Знаменатель не может быть меньше или равен нулю");
+        this.denominator = denominator;
+    }
+
+    public Fraction sum(Fraction f) {
+        numerator = numerator * f.getDenominator() + f.getNumerator() * denominator;
+        denominator *= f.getDenominator();
+        reduce();
+        return this;
+    }
+    public Fraction sum(int number) {
+        Fraction f = new Fraction(number, 1);
+        return sum(f);
+    }
+
+    public Fraction minus(Fraction f) {
+        numerator = numerator * f.getDenominator() - f.getNumerator() * denominator;
+        denominator *= f.getDenominator();
+        reduce();
+        return this;
+    }
+
+    public Fraction minus(int number) {
+        Fraction f = new Fraction(number, 1);
+        return minus(f);
+    }
+
+    private void reduce() {
+        int gcd = gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,38 +2,14 @@ import java.util.List;
 
 public class MainClass {
     public static void main(String[] args) {
-        Point p1 = new Point(1,5);
-        Point p2 = new Point(2,8);
-        Point p3 = new Point(5,3);
-        Point p4 = new Point(8,9);
 
-        Line l1 = new Line(p1, p2);
-        Line l2 = new Line(p3, p4);
-        Line l3 = new Line(l1.end, l2.start);
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 5);
+        Fraction f3 = new Fraction(7, 8);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
 
-        PolyLine pl1 = new PolyLine();
-        pl1.addPoint(p1);
-        pl1.addPoint(p2);
-        pl1.addPoint(p3);
-        pl1.addPoint(p4);
-
-
-        System.out.println(pl1.getLines());
-
-        System.out.println("Длина ломаной линии: " + pl1.getLength());
-
-        List<Line> lines = pl1.getLines();
-        double sumOfLines =  0.0;
-        for (Line line : lines) {
-            sumOfLines = sumOfLines  + line.getLength();
-        }
-        System.out.println("Сумма длин  линий: " + sumOfLines);
-        System.out.println(pl1.getLength()==sumOfLines);
-
-        p2.x= 12;
-        p2.y= 8;
-        System.out.println("Новые координаты точки p2: " + p2);
-        System.out.println("Длина ломаной линии после изменения координат: " + pl1.getLength());
-        System.out.println(pl1.getLines());
+        System.out.println(f1.sum(f2).sum(f3).minus(5));
     }
 }
